d245bf56a6de5b043fd94c19de89a23aad9582bf
genildocs <genildocs@gmail.com>
2022-07-05 18:06:25 -0300

    exlucido o index

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function pesquisarUsuario(){
-
-    const user_id = document.getElementById("user_id").value
-
-    const server_url = `https://jsonplaceholder.typicode.com/users/${user_id}`
-
-    fetch(server_url)
-    .then((server_response)=> server_response.json())
-    .then((user_json) => {
-        document.getElementById("user_name").innerHTML = "Nome: " + user_json.name
-        document.getElementById("user_email").innerHTML = "Email: " + user_json.email
-        document.getElementById("user_phone").innerHTML = "Phone: " + user_json.phone
-        document.getElementById("company").innerHTML = "Empresa: " + user_json.company["name"]
-        console.log(user_json)
-    })
-
-    
-
-  }
-
   function buscaCep(){
     const busca_cep = document.getElementById("busca_cep").value
 
