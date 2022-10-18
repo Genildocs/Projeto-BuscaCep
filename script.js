b1b66e2303baaae07d6e8b3afbc1568c258f9ef5
Genildocs <genildocs@gmail.com>
2022-10-18 13:18:22 -0300

    adicionado cotacao moedas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- /* function buscaCep(){
+ function buscaCep(){
     const busca_cep = document.getElementById("busca_cep").value
 
     const cep_url = `https://viacep.com.br/ws/${busca_cep}/json/`
@@ -25,5 +25,25 @@
       document.getElementById("city").innerHTML = ""
       
   }
-*/
-  
+
+const url = 'https://economia.awesomeapi.com.br/last/'
+const coins = 'USD-BRL,EUR-BRL,BTC-BRL'
+
+fetch(url + coins)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+      const dolarReal = data.USDBRL
+
+      let estaData = new Date(dolarReal.create_date)
+
+      document.getElementById('title').innerHTML = dolarReal.name
+      document.getElementById('thisdate').innerHTML = estaData.toLocaleString()
+      document.getElementById('valorAtul').innerHTML = parseFloat(dolarReal.high).toLocaleString('pt-br',{
+        style: 'currency',
+        currency: 'BRL'})
+    
+    })
+
+    alert('Site em constante atualização...')
